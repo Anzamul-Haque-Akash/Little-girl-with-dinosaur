0f00d0929e38564dee9ca4b3b5fac59031dbae21
Abu Obaida Raiyan <abraiyan20@gmail.com>
2023-02-01 18:19:15 +0600

    Updates

--- Assets/_Scripts/Player/GirlController.cs
+++ Assets/_Scripts/Player/GirlController.cs
@@ -1,18 +1,69 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GirlController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, BoxGroup("Target Points")] private Transform m_TargetPoint1;
+    [SerializeField] private Transform m_BoneTarget;
+    [SerializeField] private Transform m_GirlMesh;
+    private Animator _animator;
+    
+    private List<Rigidbody> rigidbodies;
+    
+    private static readonly int SeatPosition = Animator.StringToHash("SeatPosition");
+
+    public void ActiveRagdoll(bool state)
     {
-        
+        _animator.enabled = !state;
+            
+        foreach (Rigidbody r in rigidbodies)
+        {
+            r.isKinematic = !state;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        rigidbodies = m_GirlMesh.GetComponentsInChildren<Rigidbody>().Where(r => r.useGravity).ToList();
+        GirlSequence();
+    }
+
+    private void GirlSequence()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMove(m_TargetPoint1.position, 3f).SetEase(Ease.Linear));
+    }
+
+    public void GetEatByDino(Transform parent)
+    {
+        _animator.SetTrigger(SeatPosition);
+        transform.parent = parent;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = quaternion.identity;
+
+        // ActiveRagdoll(false);
+        // _animator.enabled = false;
+        // transform.parent = parent;
+        //
+        // DOVirtual.Float(0f, 1f, 0.5f, delegate(float value)
+        // {
+        //     transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, value);
+        // });
+        //
+        // DOVirtual.DelayedCall(1f, delegate
+        // {
+        //     m_BoneTarget.parent = parent;
+        //     m_BoneTarget.localPosition = Vector3.zero;
+        //     ActiveRagdoll(true);
+        // });
     }
 }
--- Assets/_Scripts/Rex/RexController.cs
+++ Assets/_Scripts/Rex/RexController.cs
@@ -9,15 +9,20 @@ namespace Rex
     public class RexController : MonoBehaviour
     {
         [SerializeField, BoxGroup("Target Points")] private Transform m_FirstTargetPoint;
-        
+        [SerializeField] private Transform m_MouthTrasform;
         private Animator _animator;
         private NavMeshAgent _agent;
         private static readonly int Roar = Animator.StringToHash("Roar");
+        private static readonly int Eat1 = Animator.StringToHash("Eat_1");
+        private static readonly int Eat2 = Animator.StringToHash("Eat_2");
+
+        private GirlController _girlController;
 
         private void Awake()
         {
             _agent = GetComponent<NavMeshAgent>();
             _animator = GetComponent<Animator>();
+            _girlController = FindObjectOfType<GirlController>();
         }
 
         private void Start()
@@ -34,19 +39,34 @@ namespace Rex
                 _agent.SetDestination(m_FirstTargetPoint.position);
             });
 
-            float waitDuration = (m_FirstTargetPoint.position - transform.position).magnitude / _agent.speed - 0.2f;
+            float waitDuration = (m_FirstTargetPoint.position - transform.position).magnitude / _agent.speed - 0.35f;
             sequence.AppendInterval(waitDuration);
             sequence.AppendCallback(delegate
             {
                 _animator.SetTrigger(Roar);
                 CameraController.Instance.SwitchToEatingCam();
             });
+            sequence.AppendInterval(3.2f);
+            sequence.AppendCallback(delegate
+            {
+                _animator.SetTrigger(Eat1);
+            });
+            sequence.AppendInterval(.5f);
+            sequence.AppendCallback(delegate
+            {
+                _girlController.GetEatByDino(m_MouthTrasform);
+            });
+            sequence.AppendInterval(.3f);
+            sequence.AppendCallback(delegate
+            {
+                _animator.SetTrigger(Eat2);
+            });
         }
 
         [UsedImplicitly]
         private void CameraShake()
         {
-            CameraController.Instance.DoShake(2f);
+            CameraController.Instance.DoShake(2.2f);
         }
     }
 }
